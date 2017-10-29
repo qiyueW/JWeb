1a1dd043340f6da0309c9635b853a2ffaf1e5959
jweb <jweb@DESKTOP-PR5BOFP>
2017-10-29 16:29:20 +0800

    Merge origin/master
    Conflicts:
    	src/java/power/hm/PowerWebServer.java
    	src/java/power/hm/role/RoleSelect.java
    	src/java/power/hm/role/validate/RolePowerValidate.java
    	src/java/power/hm/user/SetUserPower.java
    	src/java/power/hm/user/UserAU.java
    	src/java/power/hm/user/UserDell.java
    	src/java/power/hm/user/UserSelect.java
    	src/java/power/hm/user/validate/UserPowerValidate.java
    	src/java/power/powerdata/B_J.java
    	src/java/power/service/UserService.java

--- src/main/java/system/base/date/DateService.java
+++ src/main/java/system/base/date/DateService.java
@@ -1,31 +1,32 @@
 package system.base.date;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 final public class DateService {
-	public static final NextMonth NT;
-	public static final ToXDate TO;
-	public static final RunDate RUN;
-	
-	
-	static {
-		NT = new NextMonth();
-		TO=new ToXDate();
-		RUN=new RunDate();	
-	}
-	
-        final public static String getNowTime(){
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        }
-        final public static String getNowTime(String timeFormat){
-            return new SimpleDateFormat(timeFormat).format(new Date());
-        }
-        
-	public static void main(String args[]){
-		
-		System.out.println(DateService.NT.nextMonth(DateService.TO.toDate("2017-3-31")).toString());
-	}
+
+    public static final NextMonth NT;
+    public static final ToXDate TO;
+    public static final RunDate RUN;
+    public static final ScopeDate SCOPE;
+
+    static {
+        NT = new NextMonth();
+        TO = new ToXDate();
+        RUN = new RunDate();
+        SCOPE = new ScopeDate();
+    }
+
+    final public static String getNowTime() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    }
+
+    final public static String getNowTime(String timeFormat) {
+        return new SimpleDateFormat(timeFormat).format(new Date());
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(DateService.NT.nextMonth(DateService.TO.toDate("2017-3-31")).toString());
+    }
 }
