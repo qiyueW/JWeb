029d54ef96780a0f02383e77677ababffed235de
jweb <jweb@DESKTOP-PR5BOFP>
2017-10-29 15:17:49 +0800

    Merge origin/master
    Conflicts:
    	src/java/power/hm/PowerWebServer.java
    	src/java/power/hm/role/RoleSelect.java
    	src/java/power/hm/role/validate/RolePowerValidate.java
    	src/java/power/hm/user/SetUserPower.java
    	src/java/power/hm/user/UserAU.java
    	src/java/power/hm/user/UserDell.java
    	src/java/power/hm/user/UserSelect.java
    	src/java/power/hm/user/validate/UserPowerValidate.java
    	src/java/power/powerdata/B_J.java
    	src/java/power/service/UserService.java

--- src/main/java/system/web/vo/VOEngine.java
+++ src/main/java/system/web/vo/VOEngine.java
@@ -14,7 +14,7 @@ import system.web.vo.model.VO_OneModel;
  *
  * @author wangchunzi
  */
-public class VOEngine extends VO_Father implements VO_OneModel, VO_BeanModel, VO_Tool {
+public class VOEngine extends VO_Father implements VO_OneModel, VO_BeanModel,VO_Tool {
 
     javax.servlet.http.HttpServletRequest request;
 
@@ -180,7 +180,6 @@ public class VOEngine extends VO_Father implements VO_OneModel, VO_BeanModel, VO
     /**
      * 默认取 reqParam1参与reqParam2参 的值。然后组成时间范围的sql条件语句。
      *
-     * @param jw JWeb对象
      * @param qz 在返回字符串前 添加的字符串
      * @param sqlField 数据库表中某一字段。
      * @param defaultValue 默认返回，当取不到用户的日期传参时。
@@ -189,28 +188,27 @@ public class VOEngine extends VO_Father implements VO_OneModel, VO_BeanModel, VO
      * @return
      */
     @Override
-    public String getDate(JWeb jw, String qz, String sqlField, String defaultValue, String reqParam1, String reqParam2) {
-        String d1 = jw.getString(reqParam1);
-        String d2 = jw.getString(reqParam2);
-        return tDateToSQL(jw, qz, sqlField, defaultValue, d1, d2);
+    public String getDateScope(String qz, String sqlField, String defaultValue, String reqParam1, String reqParam2) {
+        String d1 =  request.getParameter(reqParam1);
+        String d2 =  request.getParameter(reqParam2);
+        return tDateToSQL(qz, sqlField, defaultValue, d1, d2);
     }
 
     /**
      * 默认取 paramkeyDate1参与paramkeyDate2参 的值。然后组成时间范围的sql条件语句。
      *
-     * @param jw JWeb对象
      * @param qz 在返回字符串前 添加的字符串
      * @param sqlField 数据库表中某一字段。
      * @return
      */
     @Override
-    public String getDate(JWeb jw, String qz, String sqlField) {
-        String d1 = jw.getString("paramkeyDate1");
-        String d2 = jw.getString("paramkeyDate2");
-        return tDateToSQL(jw, qz, sqlField, null, d1, d2);
+    public String getDateScope(String qz, String sqlField) {
+        String d1 = request.getParameter("paramkeyDate1");
+        String d2 = request.getParameter("paramkeyDate2");
+        return tDateToSQL(qz, sqlField, null, d1, d2);
     }
 
-    private static String tDateToSQL(JWeb jw, String qz, String sqlField, String defaultValue, String d1, String d2) {
+    private static String tDateToSQL(String qz, String sqlField, String defaultValue, String d1, String d2) {
         Date date1 = null, date2 = null;
         boolean b1 = false, b2 = false;
         if (!(null == d1 || d1.isEmpty())) {
