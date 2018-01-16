199f6ad4123d550c806dbbca502685e8dc2c532d
adm.wangchunzi <adm.wangchunzi@IT2.raronesz.com>
2018-01-16 20:25:17 +0800

    1

--- src/main/java/system/base/date/DateService.java
+++ src/main/java/system/base/date/DateService.java
@@ -25,6 +25,17 @@ final public class DateService {
         return new SimpleDateFormat(timeFormat).format(new Date());
     }
 
+    /**
+     *  
+     * @param date
+     * @param format 例 yyyy-MM-dd HH:mm:ss 
+     * @return 
+     */
+    final public static String getDate(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+
     public static void main(String args[]) {
 
         System.out.println(DateService.NT.nextMonth(DateService.TO.toDate("2017-3-31")).toString());
